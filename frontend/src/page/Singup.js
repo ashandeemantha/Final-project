50be724a7c19c6e445b93ade09e933f4b52462d6
Ashan Deemantha <136687325+ashandeemantha@users.noreply.github.com>
2025-03-03 23:10:21 +0530

    initial commit

--- frontend/src/page/Singup.js
+++ frontend/src/page/Singup.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import loginSignupImage from "../assest/login.gif"
 
 const Singup = () => {
   return (
-    <div>Singup</div>
+    <div className='p-3 md:p-4'>
+      <div className='w-full max-w-sm bg-white m-auto flex items-center flex-col p-4'>
+        {/* <h1 className='text-center text-2xl font-bold'> Sign up</h1> */}
+
+        <div className='w-20'>
+          <img src={loginSignupImage} className='w-full' />
+        </div>
+      </div>
+    </div>
   )
 }
 
