abc9cb0379bdb9ee4837cbfe8ff2b16f349581e6
Ashan Deemantha <136687325+ashandeemantha@users.noreply.github.com>
2025-03-05 00:01:30 +0530

    initial commit

--- frontend/src/page/Singup.js
+++ frontend/src/page/Singup.js
@@ -1,15 +1,49 @@
 import React from 'react'
 import loginSignupImage from "../assest/login.gif"
+import {BiShow,BiHide} from "react-icons/bi"
 
-const Singup = () => {
+function Singup() {
+  const [showPassword, setShowPassword] = React.useState(false)
+  const [showConfirmPassword, setShowConfirmPassword] = React.useState(false)
+  const handleShowPassword = ()=>{
+    setShowPassword(preve=> !preve)
+  }
+  const handleShowConfirmPassword = ()=>{
+    setShowConfirmPassword(preve=> !preve)
+  }
   return (
     <div className='p-3 md:p-4'>
       <div className='w-full max-w-sm bg-white m-auto flex items-center flex-col p-4'>
         {/* <h1 className='text-center text-2xl font-bold'> Sign up</h1> */}
 
-        <div className='w-20'>
+        <div className='w-20 overflow-hidden rounded-full drop-shadow-md shadow-md'>
           <img src={loginSignupImage} className='w-full' />
         </div>
+
+        <form className='w-full py-3'>
+          <label htmlFor='firstName'>First Name</label>
+          <input type={"text"} id="firstName" name='firstName' className='mt-1 mb-2 w-full bg-slate-200 px-2 py-1 rounded focus-within:outline-blue-300'/>
+
+          <label htmlFor='lastName'>Last Name</label>
+          <input type={"text"} id="lastName" name='lastName' className='mt-1 mb-2 w-full bg-slate-200 px-2 py-1 rounded  focus-within:outline-blue-300'/>
+
+          <label htmlFor='email'>Email</label>
+          <input type={"email"} id='email' name='email' className='mt-1 mb-2 w-full bg-slate-200 px-2 py-1 rounded  focus-within:outline-blue-300'/>
+
+          <label htmlFor='password'>Password</label>
+          <div className='flex px-2 py-1 bg-slate-200 rounded mt-1 mb-2 focus-within:outline focus-within:outline-blue-300'>
+          <input type={showPassword ? "text":"password"} id='password' name='password' className=' w-full bg-slate-200 border-none outline-none'/>
+          <span className='flex text-xl cursor-pointer' onClick={handleShowPassword}>{showPassword ?<BiShow/>:<BiHide/>}</span>
+          </div>
+
+          <label htmlFor='confirmpassword'>Confirm Password</label>
+          <div className='flex px-2 py-1 bg-slate-200 rounded mt-1 mb-2 focus-within:outline focus-within:outline-blue-300'>
+          <input type={showConfirmPassword ? "text":"password"} id='password' name='password' className=' w-full bg-slate-200 border-none outline-none'/>
+          <span className='flex text-xl cursor-pointer' onClick={handleShowConfirmPassword}>{showConfirmPassword ?<BiShow/>:<BiHide/>}</span>
+          </div>
+
+          <button className="w-full max-w-[150px] m-auto bg-red-300 hover:bg-red-500 cursor-pointer">Sign Up</button>
+        </form>
       </div>
     </div>
   )
